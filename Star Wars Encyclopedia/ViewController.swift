70248e0ec9a4f215f46e8449cf9eced8a981af54
rvronski <rvronski@gmail.com>
2023-08-14 22:30:41 +0500

    Initial Commit

---
+++ Star Wars Encyclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Star Wars Encyclopedia
+//
+//  Created by ROMAN VRONSKY on 14.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
